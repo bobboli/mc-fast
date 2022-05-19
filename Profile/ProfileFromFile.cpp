12a04fdf288ec3f4aaff124cf4abc6989b868caf
Yitian Ma <mkochab@sjtu.edu.cn>
2022-05-19 22:55:07 +0200

    find some ancient volumetric data

--- Profile/ProfileFromFile.cpp
+++ Profile/ProfileFromFile.cpp
@@ -1,6 +1,9 @@
 ﻿#include "Profile.h"
+#include <intrin.h>
 
-void SetupFromFile(MarchingCubes & mc, string filename)
+// regular text file
+// first 3 int as resolution x y z, then float data
+void SetupFromFile(MarchingCubes & mc, char* filename)
 {
     ifstream ifs;
     ifs.open(filename, ios::in);
@@ -30,18 +33,76 @@ void SetupFromFile(MarchingCubes & mc, string filename)
     }
 }
 
+// vol file from https://web.cs.ucdavis.edu/~okreylos/PhDStudies/Spring2000/ECS277/
+// big_endian binary
+void SetupFromVolFile(MarchingCubes& mc, char* filename)
+{
+    ifstream ifs;
+    ifs.open(filename, ios::in | ios::binary);
+    uint32_t resX, resY, resZ;
+    uint32_t big_endian;
+    ifs.read((char *)&big_endian, sizeof(uint32_t));
+    // gcc use uint32_t __builtin_bswap32 (uint32_t x)
+    resX = _byteswap_ulong(big_endian);
+    ifs.read((char*)&big_endian, sizeof(uint32_t));
+    resY = _byteswap_ulong(big_endian);
+    ifs.read((char*)&big_endian, sizeof(uint32_t));
+    resZ = _byteswap_ulong(big_endian);
+    mc.setup(resX, resY, resZ, 2000000);
+
+    // useless data
+    ifs.read((char*)&big_endian, sizeof(uint32_t));
+    uint32_t dummy = _byteswap_ulong(big_endian);
+
+    float a;
+    ifs.read((char*)&a, sizeof(float));
+    ifs.read((char*)&a, sizeof(float));
+    ifs.read((char*)&a, sizeof(float));
+
+    
+    cout << resX << " " << resY << " " << resZ << endl;
+
+    for (int i = 0; i < resX; i++)
+    {
+        for (int j = 0; j < resY; j++)
+        {
+            for (int k = 0; k < resZ; k++)
+            {
+                int c;
+                ifs.read((char*)&c, 1);
+                mc.setIsoValue(i, j, k, c);
+            }
+        }
+    }
+    ifs.close();
+}
+
 int main(int argc, char** argv)
 {
-    if (argc != 3) { printf("usage: <filename> <threshold>. \n"); return -1; }
-    string filename = argv[1];
+    if (argc != 3) { printf("usage: <filepath> <threshold>. \n"); return -1; }
+    char* filepath = argv[1];
     float threshold = atof(argv[2]);
 
     // 1. baseline result
     MarchingCubes mc;
     //const float radius = 0.4;
-    SetupFromFile(mc, filename);
+    int len = strlen(filepath);
+    char ext[4];
+    ext[0] = filepath[len - 3];
+    ext[1] = filepath[len - 2];
+    ext[2] = filepath[len - 1];
+    ext[3] = '\0';
+   
+    if(strcmp(ext, "vol") == 0)
+    {
+        SetupFromVolFile(mc, filepath);
+    }
+    else
+    {
+        SetupFromFile(mc, filepath);
+    }
     mc.update(threshold);
-    mc.exportObj("Fluid");
+    mc.exportObj("OutputMesh");
     
     // 2. Blocking result
     //MarchingCubes mc_b;
